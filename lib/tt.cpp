71757f325ff1e7dba40769c58a200eaa068e835a
Patrick Roberts <git@patrickroberts.dev>
2024-09-02 16:44:57 -0500

    Further simplified TMP for ones and zeros

--- lib/tt.cpp
+++ lib/tt.cpp
@@ -5,8 +5,7 @@
 #include <tt/core/layout.hpp>
 #include <tt/core/tensor.hpp>
 #include <tt/operators/arange.hpp>
-#include <tt/operators/ones.hpp>
-#include <tt/operators/zeros.hpp>
+#include <tt/operators/full.hpp>
 
 #include <boost/mp11.hpp>
 #include <magic_enum.hpp>
@@ -17,31 +16,28 @@
 #include <format>
 #include <memory>
 
-constexpr auto name_of(tt::Float32) { return "Float32"; }
-constexpr auto name_of(tt::Float64) { return "Float64"; }
-constexpr auto name_of(tt::BFloat16) { return "BFloat16"; }
-constexpr auto name_of(tt::UInt8) { return "UInt8"; }
-constexpr auto name_of(tt::Int8) { return "Int8"; }
-constexpr auto name_of(tt::Int16) { return "Int16"; }
-constexpr auto name_of(tt::Int32) { return "Int32"; }
-constexpr auto name_of(tt::Int64) { return "Int64"; }
-constexpr auto name_of(tt::Bool) { return "Bool"; }
+consteval auto name_of(tt::Float32) { return "Float32"; }
+consteval auto name_of(tt::Float64) { return "Float64"; }
+consteval auto name_of(tt::BFloat16) { return "BFloat16"; }
+consteval auto name_of(tt::UInt8) { return "UInt8"; }
+consteval auto name_of(tt::Int8) { return "Int8"; }
+consteval auto name_of(tt::Int16) { return "Int16"; }
+consteval auto name_of(tt::Int32) { return "Int32"; }
+consteval auto name_of(tt::Int64) { return "Int64"; }
+consteval auto name_of(tt::Bool) { return "Bool"; }
 
-constexpr auto name_of(tt::dims<0>) { return "Scalar"; }
-constexpr auto name_of(tt::dims<1>) { return "Vector"; }
-constexpr auto name_of(tt::dims<2>) { return "Matrix"; }
-constexpr auto name_of(tt::dims<3>) { return "Tensor3D"; }
-constexpr auto name_of(tt::dims<4>) { return "Tensor4D"; }
-constexpr auto name_of(tt::dims<5>) { return "Tensor5D"; }
-constexpr auto name_of(tt::dims<6>) { return "Tensor6D"; }
-constexpr auto name_of(tt::dims<7>) { return "Tensor7D"; }
-constexpr auto name_of(tt::dims<8>) { return "Tensor8D"; }
+consteval auto name_of(tt::dims<0>) { return "Scalar"; }
+consteval auto name_of(tt::dims<1>) { return "Vector"; }
+consteval auto name_of(tt::dims<2>) { return "Matrix"; }
+consteval auto name_of(tt::dims<3>) { return "Tensor3D"; }
+consteval auto name_of(tt::dims<4>) { return "Tensor4D"; }
+consteval auto name_of(tt::dims<5>) { return "Tensor5D"; }
+consteval auto name_of(tt::dims<6>) { return "Tensor6D"; }
+consteval auto name_of(tt::dims<7>) { return "Tensor7D"; }
+consteval auto name_of(tt::dims<8>) { return "Tensor8D"; }
 
-constexpr auto name_of(tt::RowMajor) { return "RowMajor"; }
-constexpr auto name_of(tt::Tiled) { return "Tiled"; }
-
-template <std::size_t, class T>
-using dependent = T;
+consteval auto name_of(tt::RowMajor) { return "RowMajor"; }
+consteval auto name_of(tt::Tiled) { return "Tiled"; }
 
 PYBIND11_MODULE(tt, m) {
   namespace py = pybind11;
@@ -51,11 +47,14 @@ PYBIND11_MODULE(tt, m) {
                                          const py::handle &handle,
                                          const char *name) {
     using enum_type = T;
-
     py::enum_<enum_type> type(handle, name);
 
     for (const auto &[value, name] : magic_enum::enum_entries<enum_type>()) {
       type.value(name.data(), value);
+    }
+
+    if constexpr (magic_enum::is_unscoped_enum_v<enum_type>) {
+      type.export_values();
     }
   };
 
@@ -82,7 +81,7 @@ PYBIND11_MODULE(tt, m) {
 
   auto m_tensor = m.def_submodule("Tensor");
 
-  mp::mp_for_each<tensor_types>([&]<class T>(mp::mp_identity<T>) {
+  mp::mp_for_each<tensor_types>([&]<tt::tensor T>(mp::mp_identity<T>) {
     using tensor_type = T;
     using layout_type = tt::layout_type_t<tensor_type>;
     using element_type = tt::element_type_t<tensor_type>;
@@ -98,14 +97,18 @@ PYBIND11_MODULE(tt, m) {
 
   const auto default_dtype = std::make_shared<tt::DType>(tt::DType::Float32);
 
+  const auto value_or_default = [=](std::optional<tt::DType> dtype) {
+    return dtype ? *dtype : *default_dtype;
+  };
+
   m.def("set_default_dtype", [=](tt::DType dtype) { *default_dtype = dtype; });
 
   m.def("get_default_dtype", [=] { return *default_dtype; });
 
   constexpr auto with_element = [](tt::DType dtype, auto callback) {
     return magic_enum::enum_switch(
-        [&](auto dtype) {
-          constexpr auto index = static_cast<std::size_t>(dtype());
+        [&](tt::integral_constant_like auto dtype) {
+          constexpr auto index = *magic_enum::enum_index(dtype());
           using element_type = mp::mp_at_c<element_types, index>;
           return callback(element_type{});
         },
@@ -114,10 +117,22 @@ PYBIND11_MODULE(tt, m) {
 
   constexpr auto arange = [=](auto start, auto end, tt::Float64 step,
                               tt::DType dtype) {
-    return with_element(dtype, [=]<class TElement>(TElement) {
+    return with_element(dtype, [=]<tt::arithmetic TElement>(TElement) {
       return py::cast(tt::arange<TElement>(start, end, step));
     });
   };
+
+  m.def(
+      "arange",
+      [=](tt::Int64 end, tt::DType dtype) { return arange(0, end, 1, dtype); },
+      py::arg("end"), py::kw_only(), py::arg("dtype") = tt::DType::Int64);
+
+  m.def(
+      "arange",
+      [=](tt::Float64 end, std::optional<tt::DType> dtype) {
+        return arange(0, end, 1, value_or_default(dtype));
+      },
+      py::arg("end"), py::kw_only(), py::arg("dtype") = py::none());
 
   m.def(
       "arange",
@@ -129,28 +144,18 @@ PYBIND11_MODULE(tt, m) {
 
   m.def(
       "arange",
-      [=](tt::Int64 end, tt::DType dtype) { return arange(0, end, 1, dtype); },
-      py::arg("end"), py::kw_only(), py::arg("dtype") = tt::DType::Int64);
-
-  m.def(
-      "arange",
       [=](tt::Float64 start, tt::Float64 end, tt::Float64 step,
           std::optional<tt::DType> dtype) {
-        return arange(start, end, step, dtype ? *dtype : *default_dtype);
+        return arange(start, end, step, value_or_default(dtype));
       },
       py::arg("start"), py::arg("end"), py::arg("step") = 1, py::kw_only(),
       py::arg("dtype") = py::none());
 
-  m.def(
-      "arange",
-      [=](tt::Float64 end, std::optional<tt::DType> dtype) {
-        return arange(0, end, 1, dtype ? *dtype : *default_dtype);
-      },
-      py::arg("end"), py::kw_only(), py::arg("dtype") = py::none());
+  constexpr auto with_extents = [](const py::args &args, auto callback) {
+    constexpr mp::mp_size<element_types> size{};
 
-  constexpr auto with_extents = [=](const py::args &args, auto callback) {
-    return mp::mp_with_index<mp::mp_size<element_types>::value>(
-        args.size(), [&](auto rank) {
+    return mp::mp_with_index<size>(
+        args.size(), [&](tt::integral_constant_like auto rank) {
           return [&]<auto... Is>(std::index_sequence<Is...>) {
             return callback(py::cast<std::size_t>(args[Is])...);
           }(std::make_index_sequence<rank>{});
@@ -159,32 +164,25 @@ PYBIND11_MODULE(tt, m) {
 
   constexpr auto with_element_and_extents =
       [=](tt::DType dtype, const py::args &args, auto callback) {
-        return with_element(dtype, [&](auto element) {
-          return with_extents(args, [&](auto... extents) {
+        return with_element(dtype, [&](tt::arithmetic auto element) {
+          return with_extents(args, [&](tt::index auto... extents) {
             return callback(element, extents...);
           });
         });
       };
 
-  m.def(
-      "ones",
-      [=](const py::args &args, std::optional<tt::DType> dtype) {
-        return with_element_and_extents(
-            dtype ? *dtype : *default_dtype, args,
-            []<class TElement>(TElement, auto... extents) {
-              return py::cast(tt::ones<TElement>(extents...));
-            });
-      },
-      py::kw_only(), py::arg("dtype") = py::none());
+  const auto with_fill = [=](tt::arithmetic auto fill_value) {
+    return [=](const py::args &args, std::optional<tt::DType> dtype) {
+      return with_element_and_extents(
+          value_or_default(dtype), args,
+          [&]<tt::arithmetic TElement>(TElement, tt::index auto... extents) {
+            return py::cast(
+                tt::full(static_cast<TElement>(fill_value), extents...));
+          });
+    };
+  };
 
-  m.def(
-      "zeros",
-      [=](const py::args &args, std::optional<tt::DType> dtype) {
-        return with_element_and_extents(
-            dtype ? *dtype : *default_dtype, args,
-            []<class TElement>(TElement, auto... extents) {
-              return py::cast(tt::zeros<TElement>(extents...));
-            });
-      },
-      py::kw_only(), py::arg("dtype") = py::none());
+  m.def("ones", with_fill(1), py::kw_only(), py::arg("dtype") = py::none());
+
+  m.def("zeros", with_fill(0), py::kw_only(), py::arg("dtype") = py::none());
 }
